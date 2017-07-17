d326b3b528d17bca5bac5f1a71c2d44b5bf5a3ab
valicB <valentinbriceag@gmail.com>
2017-07-17 21:02:24 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const swig = require('swig'); //Template engines
+
+// MIDDLEWARE
+app.use(express.static('public'));
+
+// CONFIG
+app.engine('html',swig.renderFile);
+app.set('view engine','html');
+app.set('views', __dirname + '/views');
+
+//  disable CASHE
+app.set('view cache', false); // express cache
+swig.setDefaults({cache: false}); // swig cache
+
+// ROUTING
+app.get('/register', (req, res)=>{
+  res.render('register-page');
+});
+app.get('/login', (req, res)=>{
+  res.render('login-page');
+})
+
 
 app.get('/', (req, res)=> {
-  res.send('All good!')
+  res.render('homepage'); // show this template
 });
 
 app.listen(9090, function () {
