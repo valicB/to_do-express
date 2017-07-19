d4c589a092d957fb4858b3e1610d14d1caaa76a1
valicB <valentinbriceag@gmail.com>
2017-07-19 21:08:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const app = express();
-const swig = require('swig'); //Template engines
+const express     = require('express');     // library
+const app         = express();              // application
+const swig        = require('swig');        //Template engines
+const bodyParser  = require("body-parser"); // process post/get data
+const expressValidator = require('express-validator');
+
+
 
 // MIDDLEWARE
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true })) // parse application/x-www-form-urlencoded
+app.use(expressValidator());
 
 // CONFIG
 app.engine('html',swig.renderFile);
@@ -20,7 +26,55 @@ app.get('/register', (req, res)=>{
 });
 app.get('/login', (req, res)=>{
   res.render('login-page');
-})
+});
+
+app.post('/user/create', (req, res)=>{
+  // var fullname  = req.body.fullname;
+  // var email     = req.body.email;
+  // var password  = req.body.password;
+  // var cpassword = req.body.cpassword;
+
+  req.check({
+   'email': {
+      notEmpty: true,
+      isEmail: {
+        error_email: 'Invalid Email'
+      }
+    }
+  });
+  // De facut cu Result IP
+  req.assert('email', 'required').notEmpty();
+  req.assert('email', 'valid email required').isEmail();
+  req.assert('password', '6 to 20 characters required').len(6, 20);
+
+  req.getValidationResult().then(function(result) {
+    // De validat cu if si else
+    if (result.isEmpty()) {
+      res.send('All goods');
+    } else {res.send('hahaha');}
+
+  });
+  // if(fullname.length < 3){
+  //   // error
+  //   res.render('register-page', {error_fullname : 'Fullname must be at least 3 symbols'});
+  // } else if (email.length < 3) {
+  //   res.render('register-page', {error_email : 'Email must be at least 3 symbols'});
+  // }else if (password.length < 3) {
+  //   res.render('register-page', {error_password : 'Password must be at least 3 symbols'});
+  // }else if (password != cpassword) {
+  //   res.render('register-page', {error_cpass : 'The password must be the same'});
+  // }
+  //  else{
+  //   res.send('All goods');
+  // }
+
+  /// 1) sanitization
+  /// 2) validation
+  /// 3) save to database
+
+  // console.log(req.body);
+
+});
 
 
 app.get('/', (req, res)=> {
