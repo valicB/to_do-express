a21d1655dc81efbd4af8bee99d04078d9f83092d
valicB <valentinbriceag@gmail.com>
2017-07-21 15:54:58 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -34,24 +34,23 @@ app.post('/user/create', (req, res)=>{
   // var password  = req.body.password;
   // var cpassword = req.body.cpassword;
 
-  req.check({
-   'email': {
-      notEmpty: true,
-      isEmail: {
-        error_email: 'Invalid Email'
-      }
-    }
-  });
-  // De facut cu Result IP
-  req.assert('email', 'required').notEmpty();
-  req.assert('email', 'valid email required').isEmail();
+
+
+  req.assert('fullname', 'Fullname is required').notEmpty();
+  req.assert('email', 'Email is required').notEmpty();
+  req.assert('email', 'Invalid email').isEmail();
   req.assert('password', '6 to 20 characters required').len(6, 20);
+  req.assert('cpassword', 'Passwords must match').equals(req.body.password);
 
   req.getValidationResult().then(function(result) {
-    // De validat cu if si else
-    if (result.isEmpty()) {
-      res.send('All goods');
-    } else {res.send('hahaha');}
+
+    console.log(result.isEmpty());
+    if (!result.isEmpty()) {
+      // console.log(result.array()[0].param);
+      // console.log(result.array()[0].msg);
+      res.render('register-page', {error : result.array()[0].msg});
+
+    } else {res.send('All goods');}
 
   });
   // if(fullname.length < 3){
