59bc4b545095710f86dc5164370a7144ad558e41
valicB <valentinbriceag@gmail.com>
2017-07-21 21:03:28 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const app         = express();              // application
 const swig        = require('swig');        //Template engines
 const bodyParser  = require("body-parser"); // process post/get data
 const expressValidator = require('express-validator');
-
+const cookieSession = require('cookie-session');
 
 
 // MIDDLEWARE
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true })) // parse application/x-www-form-urlencoded
 app.use(expressValidator());
+app.use(cookieSession({
+  name: 's3Cur3',
+  keys: ['nizYm70O4Pqwg1LE1'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 // CONFIG
 app.engine('html',swig.renderFile);
@@ -26,6 +31,10 @@ app.get('/register', (req, res)=>{
 });
 app.get('/login', (req, res)=>{
   res.render('login-page');
+});
+app.get('/user/logout', (req, res)=>{
+  res.session = null;
+  res.redirect('/login');
 });
 
 app.post('/user/create', (req, res)=>{
@@ -73,10 +82,41 @@ app.post('/user/create', (req, res)=>{
   // console.log(req.body);
 
 });
+app.post('/user/authenticated', (req, res)=>{
+  var fullname  = req.body.fullname;
+  var email     = req.body.email;
+  var password  = req.body.password;
+
+  req.assert('email', 'Email is required').notEmpty();
+  req.assert('email', 'Invalid email').isEmail();
+  req.assert('password', '6 to 20 characters required').len(6, 20);
+
+  req.getValidationResult().then(function(result) {
+
+    console.log(result.isEmpty());
+    if (!result.isEmpty()) {
+      // console.log(result.array()[0].param);
+      // console.log(result.array()[0].msg);
+      res.render('login-page', {error : result.array()[0].msg});
+    } else {
+        if (
+          email == "admin@email.com" && password == "1234567" ||
+          email == "admin1@email.com" && password == "1234567"
+      ){
+          req.session.is_online = true;
+          req.session.email = email;
+          res.redirect('/');
+        } else res.send("Access denied")
+    }
+
+  });
+
+
+});
 
 
 app.get('/', (req, res)=> {
-  res.render('homepage'); // show this template
+  res.render('homepage', {name: "Express", onLine: req.session.is_online, email: req.session.email}); // show this template
 });
 
 app.listen(9090, function () {
